9cbacff8762a1c3a5d67a1bf2ec420289b237a8b
cristake007 <popadorincristian@gmail.com>
2026-07-19 20:05:22 +0300

    Fix PDF style resolver injection

--- DocumentBuilder/files/custom/Espo/Modules/DocumentBuilder/Tools/DocumentBuilder/Layout/ConfiguredStyleResolver.php
+++ DocumentBuilder/files/custom/Espo/Modules/DocumentBuilder/Tools/DocumentBuilder/Layout/ConfiguredStyleResolver.php
@@ -4,14 +4,15 @@ declare(strict_types=1);
 
 namespace Espo\Modules\DocumentBuilder\Tools\DocumentBuilder\Layout;
 
-use Espo\Modules\DocumentBuilder\Tools\DocumentBuilder\Config\Settings;
+use Espo\Modules\DocumentBuilder\Tools\DocumentBuilder\Config\SettingsProvider;
 
 final readonly class ConfiguredStyleResolver implements ResolvedStyleProvider
 {
     private StyleResolver $resolver;
 
-    public function __construct(Settings $settings)
+    public function __construct(SettingsProvider $settingsProvider)
     {
+        $settings = $settingsProvider->get();
         $this->resolver = new StyleResolver($settings->allowedFontList(), $settings->defaultFont());
     }
 
